5c13ffe5edb7e987a92eac40f574382e232e1aea
Leon <muchuanj@163.com>
2018-01-22 17:30:05 +0800

    add resource role

--- src/api/posts.js
+++ src/api/posts.js
@@ -8,6 +8,18 @@ function getCategory () {
   return axios.get('/api/category/list')
 }
 
+function getTag () {
+  return axios.get('/api/tag/list')
+}
+
+function save (params) {
+  return axios.post('/api/posts/save', params)
+}
+
+function get (id) {
+  return axios.get(`/api/posts/get?id=${id}`)
+}
+
 export default {
-  getList, getCategory
+  getList, getCategory, getTag, save, get
 }
--- src/main.js
+++ src/main.js
@@ -3,22 +3,24 @@ import App from './components/App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import api from './api'
+import mavonEditor from 'mavon-editor'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.css'
 import 'admin-lte/dist/css/AdminLTE.min.css'
 import 'admin-lte/dist/css/skins/_all-skins.css'
-import './assets/styles.css'
-
 import 'jquery/dist/jquery.min'
 import 'bootstrap/dist/js/bootstrap.min'
 import 'admin-lte/dist/js/adminlte.min'
 import 'admin-lte/dist/js/demo'
+import 'mavon-editor/dist/css/index.css'
+import './assets/styles.css'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI, {size: 'small'})
 
 Vue.use(api)
+Vue.use(mavonEditor)
 
 /* eslint-disable no-new */
 new Vue({
