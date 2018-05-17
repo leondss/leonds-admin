f6cb60e39b4ddae819d1a23046b48b0a9a8cc5b6
Leon <muchuanj@163.com>
2018-05-17 17:14:42 +0800

    add scss

--- src/main.js
+++ src/main.js
@@ -4,9 +4,8 @@ import router from './router'
 import ElementUI from 'element-ui'
 import api from './api'
 import mavonEditor from 'mavon-editor'
-import 'element-ui/lib/theme-chalk/index.css'
 import 'mavon-editor/dist/css/index.css'
-import './assets/styles.css'
+import './styles.scss'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI, {size: 'small'})
